1cc32a29e5be49416dc61262df8085a648476fe6
Harshdeep Singh Saini <harshdeepsinghsaini2023@gmail.com>
2026-04-29 01:13:19 +0530

    Added Module1Introduction

---
+++ Module1Introduction/src/main/java/com/codingshuttle/Module1Introduction/AppConfig.java
@@ -0,0 +1,12 @@
+package com.codingshuttle.Module1Introduction;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public PaymentService paymentService(){
+        return new PaymentService();
+    }
+}
